a3c441259fb2ff301a7f2aab3fb2e305c3736c9b
Ezfen <ezfen_zhang@163.com>
2016-03-06 21:15:03 +0800

    修改网络访问API接口

--- LibrarySeats/LibrarySeatsHeaders.m
+++ LibrarySeats/LibrarySeatsHeaders.m
@@ -15,8 +15,8 @@ NSString *const PASSWORD = @"password";
 NSString *const TOKEN = @"token";
 NSString *const LOGINDATA = @"login_data";
 
-//NSString *const WEBSITE = @"http://1.libraryseat.applinzi.com";
-NSString *const WEBSITE = @"http://192.168.1.105:8081/library";
+NSString *const WEBSITE = @"http://1.libraryseat.applinzi.com";
+//NSString *const WEBSITE = @"http://192.168.1.105:8081/library";
 
 NSString *const LibrarySeatDatabaseAvailabilityNotification = @"LibrarySeatDatabaseAvailabilityNotification";
 NSString *const LibrarySeatDatabaseAvailabilityContext = @"LibrarySeatDatabaseAvailabilityContext";
