74fb744ff3225256f46ccdddac9014dea4ecb9ab
Ezfen <ezfen_zhang@163.com>
2016-05-08 23:28:16 +0800

    修改座位icon

--- LibrarySeats/LibrarySeatsHeaders.m
+++ LibrarySeats/LibrarySeatsHeaders.m
@@ -16,7 +16,7 @@ NSString *const TOKEN = @"token";
 NSString *const LOGINDATA = @"login_data";
 
 //NSString *const WEBSITE = @"http://1.libraryseat.applinzi.com";
-NSString *const WEBSITE = @"http://192.168.1.105:8081/library";
+NSString *const WEBSITE = @"http://192.168.1.104:8081/library";
 
 NSString *const LibrarySeatDatabaseAvailabilityNotification = @"LibrarySeatDatabaseAvailabilityNotification";
 NSString *const LibrarySeatDatabaseAvailabilityContext = @"LibrarySeatDatabaseAvailabilityContext";
